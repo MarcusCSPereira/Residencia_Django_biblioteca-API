e63e9ef60d6049fbf2742c9f5e967c027edecb59
Marcus César Santos Pereira <marcus.cesar.sp@gmail.com>
2024-10-04 21:35:10 -0300

    refactor(api): adiciona comentários simples em algumas características do código

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
 from . import views
+from rest_framework.routers import SimpleRouter
+
+#router = SimpleRouter()
+#Só usamos routers quando usamos modelViewSet
 
 urlpatterns = [
+    #path("", include(router.urls)),
     path("", views.APIRootView.as_view(), name='api-root'),
     path("books/", views.BookList.as_view(), name=views.BookList.name),
     path("books/<int:pk>/", views.BookDetail.as_view(), name=views.BookDetail.name),
